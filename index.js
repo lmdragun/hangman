506eb023d669ba8577486caaa2e2616265123269
Lindsey <lmdragun@gmail.com>
2015-05-21 15:14:57 -0400

    io initial setup

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
-var io = require("socket.io")(http);
+var io = require('socket.io');
+var io = io(http);
+// var io = require("socket.io")(http);
 
 mongoose.connect("mongodb://localhost/hangman");
 var db = mongoose.connection;
 var game = mongoose.model("Game", new mongoose.Schema({
   word: String,
-  Guesses: [],
+  Guesses: {right:[], wrong:[]},
   Man: [],
 	Status: Boolean
 	})
 );
 
-app.listen(3000, function(){
+http.listen(3000, function(){
   console.log("Listening on port 3000");
 });
