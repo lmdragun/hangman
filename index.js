5377229bb453fcdca8fa7ee7863066310848974e
Lindsey <lmdragun@gmail.com>
2015-05-21 15:36:48 -0400

    initial stuff

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var io = require('socket.io');
-var io = io(http);
-// var io = require("socket.io")(http);
+var io = require('socket.io')(http);
 
 mongoose.connect("mongodb://localhost/hangman");
 var db = mongoose.connection;
@@ -16,6 +14,16 @@ var game = mongoose.model("Game", new mongoose.Schema({
 	Status: Boolean
 	})
 );
+
+app.set("view engine", "hbs");
+
+app.get('/', function(req, res){
+  res.render("index");
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 http.listen(3000, function(){
   console.log("Listening on port 3000");
